ad27af4db4b12ccbb6c23218e4732776f7810c5f
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-03-13 09:03:10 -0400

    types

--- src/main/java/Types.java
+++ src/main/java/Types.java
@@ -3,11 +3,17 @@ import org.omg.CORBA.PUBLIC_MEMBER;
 import java.util.Comparator;
 
 public enum Types {
+    NORMAL("GHOST", "FIGHTING"),//normal not strong against anything but is resistant to ghost
     FIRE("GRASS","WATER"),
     WATER("FIRE", "GRASS"),
+    ELECTRIC("WATER", "GROUND"),
     GRASS("WATER", "FIRE"),
-    GHOST("GHOST", "NORMAL"),
-    NORMAL("GHOST", "FIGHTING");//normal not strong against anything but is resistant to ghost
+    ICE("GRASS", "FIRE"),
+    FIGHTING("NORMAL","FLYING"),
+    POISON("GRASS", "GROUND"),
+    //GROUND("")
+    GHOST("GHOST", "NORMAL");
+
     String strongAgainst;
     String weakAgainst;
 
--- src/main/java/YoungsterJoey.java
+++ src/main/java/YoungsterJoey.java
@@ -3,5 +3,6 @@ import java.util.List;
 public class YoungsterJoey extends Trainers {
     public YoungsterJoey(String name, List<Pokemon> team) {
         super(name, team);
+
     }
 }
