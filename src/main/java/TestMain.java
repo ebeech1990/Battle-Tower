4f75e7cf1693e07742de3f1b7547cf6b86817835
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-03-23 18:57:16 -0400

    able to retrieve json

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,14 +1,15 @@
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+//import com.google.gson.JsonParser;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
@@ -18,15 +19,36 @@ import net.sf.json.JSONSerializer;
 import org.apache.commons.io.IOUtils;
 
 import javax.json.JsonArray;
-import javax.json.JsonObject;
 
 public class TestMain {
 
     public static void main(String[] args) throws IOException, ParseException {
-        JSONObject obj = new JSONObject(IOUtils.toString(new URL(jsonlink), Charset.forName("UTF-8")));
-        JSONObject facet_counts = obj.getJSONObjecy("facet_counts");
-        JSONObject facet_fields = facet_counts.getJSONObjecy("facet_fields");
-        JSONArray test_array = obj.getJSONArray("TESTARRAY");
-        Integer smithage = test_array.getInt(3);
+
+        String result = downloadWebPage("https://pokeapi.co/api/v2/pokemon-species/bulbasaur/");
+        System.out.println(result);
+    }
+    private static String downloadWebPage(String url) throws IOException {
+        System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
+        StringBuilder result = new StringBuilder();
+        String line;
+
+
+        URLConnection urlConnection = new URL(url).openConnection();
+        urlConnection.addRequestProperty("User-Agent", "Mozilla");
+
+        urlConnection.setReadTimeout(5000);
+        urlConnection.setConnectTimeout(5000);
+
+        try (InputStream is = new URL(url).openStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+
+        }
+
+        return result.toString();
+
     }
 }
